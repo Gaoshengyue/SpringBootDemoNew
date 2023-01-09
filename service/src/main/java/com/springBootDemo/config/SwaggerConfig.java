191c200c8dbbe86c9de7372604151d8ae7441401
gaoshengyue@sinowel.com <597426245@qq.com>
2023-01-09 15:33:13 +0800

    feat(README):添加swagger

---
+++ service/src/main/java/com/springBootDemo/config/SwaggerConfig.java
@@ -0,0 +1,107 @@
+package com.springBootDemo.config;
+
+import java.lang.reflect.Field;
+import java.util.List;
+
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.annotation.Bean;
+
+import org.springframework.util.ReflectionUtils;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfoHandlerMapping;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.spring.web.plugins.WebFluxRequestHandlerProvider;
+import springfox.documentation.spring.web.plugins.WebMvcRequestHandlerProvider;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.stream.Collectors;
+
+/**
+ * Description:
+ * ========================================================================
+ * 韩峥
+ * ------------------------------------------------------------------------
+ *
+ * @author hanzheng
+ * @version 1.0
+ * <p>
+ * ========================================================================
+ * @date 创建于 13/07/2022   13:49
+ */
+
+@EnableSwagger2
+@Configuration   // 相当于Spring配置中的<beans>
+@EnableKnife4j
+public class SwaggerConfig implements WebMvcConfigurer {
+    @Bean
+    public static BeanPostProcessor springfoxHandlerProviderBeanPostProcessor() {
+        return new BeanPostProcessor() {
+
+            @Override
+            public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+                if (bean instanceof WebMvcRequestHandlerProvider || bean instanceof WebFluxRequestHandlerProvider) {
+                    customizeSpringfoxHandlerMappings(getHandlerMappings(bean));
+                }
+                return bean;
+            }
+
+            private <T extends RequestMappingInfoHandlerMapping> void customizeSpringfoxHandlerMappings(List<T> mappings) {
+                List<T> copy = mappings.stream()
+                        .filter(mapping -> mapping.getPatternParser() == null)
+                        .collect(Collectors.toList());
+                mappings.clear();
+                mappings.addAll(copy);
+            }
+
+            @SuppressWarnings("unchecked")
+            private List<RequestMappingInfoHandlerMapping> getHandlerMappings(Object bean) {
+                try {
+                    Field field = ReflectionUtils.findField(bean.getClass(), "handlerMappings");
+                    field.setAccessible(true);
+                    return (List<RequestMappingInfoHandlerMapping>) field.get(bean);
+                } catch (IllegalArgumentException | IllegalAccessException e) {
+                    throw new IllegalStateException(e);
+                }
+            }
+        };
+    }
+
+    @Bean   // 相当于Spring 配置中的<bean>
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.OAS_30)
+                .groupName("蜂巢接口")
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.ufin.honeycomb.controller"))
+                .paths(PathSelectors.any())
+                .build();
+
+    }
+
+    // API基础信息定义（就是更新Swagger默认页面上的信息）
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("蜂巢接口开发文档")
+                .description("文档描述：蜂巢接口开发文档")
+                .contact(new Contact("蜂巢开发", "作者网站(url)", ""))
+                .version("1.0")
+                .build();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("doc.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+
+}
